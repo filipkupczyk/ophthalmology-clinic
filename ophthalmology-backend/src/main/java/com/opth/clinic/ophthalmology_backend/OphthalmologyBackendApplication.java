5ff865ebeabb61baee6819980dd8137d9d4582b6
Filip <robertfilip17@gmail.com>
2026-01-06 17:26:25 +0100

    Adding frontend and backend projects

---
+++ ophthalmology-backend/src/main/java/com/opth/clinic/ophthalmology_backend/OphthalmologyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.opth.clinic.ophthalmology_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OphthalmologyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OphthalmologyBackendApplication.class, args);
+        System.out.println("jebac legie");
+	}
+}
